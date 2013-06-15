830155d74fb7738f58da05804651046ef57d832f
yuanxin <yuanxin.qiu@easyread.ly>
2013-06-15 19:32:24 +0800

    Fix resource path

--- config.rb
+++ config.rb
@@ -4,10 +4,10 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/movie/"
+http_images_path = "/movie/images"
+http_fonts_path = "/movie/fonts"
+css_dir = "public/movie/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
